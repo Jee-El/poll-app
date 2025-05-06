673bc4e6da7518e85aa7e9f9fdb0de977739020f
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-06 17:19:40 +0100

    feat: create Question & Choice models

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_txt = models.CharField(max_length=90)
+    description = models.CharField(max_length=220)
+    created_at = models.DateTimeField("Published on")
+    deadline = models.DateTimeField("Ends on")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=50)
+    votes = models.PositiveIntegerField(default=0)
