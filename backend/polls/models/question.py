a0a4b50e855b07f95ea4688b6959effeaba57674
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-07 18:59:42 +0100

    feat: add default value for created_at field

--- backend/polls/models/question.py
+++ backend/polls/models/question.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_txt = models.CharField(max_length=90)
     description = models.CharField(max_length=220)
-    created_at = models.DateTimeField("Published on")
+    created_at = models.DateTimeField("Published on", default=timezone.now)
     deadline = models.DateTimeField("Ends on")
 
     def __str__(self):
