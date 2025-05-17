16a276eebd2e89866dced62fb573ffba9c1e118f
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-17 20:32:19 +0100

    chore: add simplejwt & corsheaders to installed apps

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
     "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
     "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
