8ed4225f287ff41385da7043521aae758a1b9772
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-21 20:54:09 +0100

    style: rename votes to vote_count for clarity

--- backend/polls/models/choice.py
+++ backend/polls/models/choice.py
@@ -5,7 +5,9 @@ from .question import Question
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_txt = models.CharField(max_length=50)
-    votes = models.PositiveIntegerField(default=0)
+    vote_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.choice_txt
+
+
