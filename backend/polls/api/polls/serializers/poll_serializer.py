02ba1dbad71e9be61b830ac4f68e9c870a2897a8
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-24 20:53:09 +0100

    feat: add deadline validation to serializer

--- backend/polls/api/polls/serializers/poll_serializer.py
+++ backend/polls/api/polls/serializers/poll_serializer.py
@@ -1,9 +1,23 @@
-from rest_framework.serializers import ModelSerializer
+from random import choice
+from rest_framework import serializers
 
 from polls.models import Poll
 
 
-class PollSerializer(ModelSerializer):
+class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = "__all__"
+
+    def validate(self, data):
+        super().validate(data)
+
+        created_at = data.get("created_at") or getattr(
+            self.instance, "created_at", None
+        )
+        deadline = data.get("deadline")
+        if created_at and deadline and (created_at >= deadline):
+            raise serializers.ValidationError(
+                "deadline must be greater than created_at."
+            )
+        return data
