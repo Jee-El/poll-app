2dc02ca0fb5a9513e8375a3ed4554272d53833fa
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-25 23:11:22 +0100

    feat: add /whoami to get authenticated user info
    - It's for basic info for the UI.
    
    - Small fix: As APPEND_SLASH is True by default, I also appended it as
    it caused errors.

--- backend/polls/api/auth/urls.py
+++ backend/polls/api/auth/urls.py
@@ -4,11 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+
+from polls.api.auth.views import whoami
 from . import views
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("refresh_token", TokenRefreshView.as_view(), name="refresh_token"),
-    path("verify_token", TokenVerifyView.as_view(), name="verify_token"),
+    path("refresh_token/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("verify_token/", TokenVerifyView.as_view(), name="verify_token"),
+    path("whoami/", views.whoami, name="whoami"),
 ]
--- backend/polls/api/auth/views/__init__.py
+++ backend/polls/api/auth/views/__init__.py
@@ -1,1 +1,2 @@
 from .signup import signup
+from .whoami import whoami
