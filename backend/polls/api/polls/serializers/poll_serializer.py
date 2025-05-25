7a9ee682a938a6ccd1f39778436ef49e0845ee88
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-25 22:29:36 +0100

    fix: use correct related_name for reverse relation
    Also removed is_active from read_only_field since that field
    was removed in the prev commit.

--- backend/polls/api/polls/serializers/poll_serializer.py
+++ backend/polls/api/polls/serializers/poll_serializer.py
@@ -2,21 +2,25 @@ from rest_framework import serializers
 from polls.models.choice import Choice
 from . import ChoiceSerializer
 from polls.models import Poll
+from django.utils import timezone
 
 
 class PollSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True)
+    choice_set = ChoiceSerializer(many=True)
+    user = serializers.SlugRelatedField(read_only=True, slug_field="username")
 
     class Meta:
         model = Poll
         fields = "__all__"
+        read_only_fields = ["user", "created_at"]
 
     def create(self, validated_data):
-        choices_data = validated_data.pop("choices")
-        poll = Poll.objects.create(**validated_data)
+        choices_data = validated_data.pop("choice_set")
 
+        poll = Poll.objects.create(**validated_data)
         for choice_data in choices_data:
             Choice.objects.create(poll=poll, **choice_data)
+
         return poll
 
     def validate(self, data):
