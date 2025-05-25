d925ba6e12fb43857cf79eddeb69a5a4e28c62eb
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-25 18:21:14 +0100

    chore: set /polls prefix rather than root for app

--- backend/polls/api/urls.py
+++ backend/polls/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("polls.api.polls.urls")),
+    path("polls/", include("polls.api.polls.urls")),
     path("auth/", include("polls.api.auth.urls")),
 ]
