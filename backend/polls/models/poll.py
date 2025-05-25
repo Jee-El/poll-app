2fa8b51f8f8473bbe516e2c412d7896ce1ec5d02
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-25 19:18:46 +0100

    feat: prevent invalid polls at the db-level
    Added database constraint to ensure polls cannot be created with
    deadlines in the past.

--- backend/polls/models/poll.py
+++ backend/polls/models/poll.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models import Q, F
 
 
 class Poll(models.Model):
     question = models.CharField(max_length=90)
-    description = models.CharField(max_length=220, default="")
+    description = models.CharField(max_length=220, default="", blank=True)
     allows_multiple_choices = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField("Published on", default=timezone.now)
     deadline = models.DateTimeField("Ends on")
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(deadline__gt=F("created_at")), name="deadline_not_in_past"
+            )
+        ]
+
     def __str__(self):
-        return self.question_txt
+        return self.question
