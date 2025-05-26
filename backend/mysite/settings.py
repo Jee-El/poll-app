2d890152454f778541e12794d3a5a1923429f337
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-26 02:06:03 +0100

    chore: remove unused simplejwt settings
    Removed sliding-tokens-related settings as sliding tokens aren't used.
    Modified access token lifetime from 60mins to 10mins.

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -62,11 +62,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-    "SLIDING_TOKEN_LIFETIME": timedelta(days=30),
-    "SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER": timedelta(days=1),
-    "SLIDING_TOKEN_LIFETIME_LATE_USER": timedelta(days=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 MIDDLEWARE = [
