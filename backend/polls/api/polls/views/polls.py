2079a7c66670e21328e5f01e9e0246bebc8b2d06
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-31 20:15:24 +0100

    feat: sort polls by descending creation date

--- backend/polls/api/polls/views/polls.py
+++ backend/polls/api/polls/views/polls.py
@@ -7,7 +7,7 @@ from polls.models import Poll
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
-def getPolls(request):
-    polls = Poll.objects.prefetch_related("choice_set").all()
+def get_polls(request):
+    polls = Poll.objects.all().prefetch_related("choice_set").order_by("-created_at")
     serializedPolls = PollSerializer(polls, many=True)
     return Response(serializedPolls.data)
