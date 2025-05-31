6be8243033cf3f8a23dcc1dae85b6cfd8f52c030
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-05-31 20:14:05 +0100

    refactor: use snake_case for method name
    going forward, i'm gonna be following PEP 8 style guide

--- backend/polls/api/polls/views/poll.py
+++ backend/polls/api/polls/views/poll.py
@@ -7,7 +7,7 @@ from polls.models import Poll
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
-def getPoll(request, id: int):
+def get_poll(request, id: int):
     poll = Poll.objects.prefetch_related("choice_set").get(pk=id)
     serializedPoll = PollSerializer(poll)
     return Response(serializedPoll.data)
